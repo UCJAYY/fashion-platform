15b1baf38b08ed84807ad54fc8cd003e9dcb7565
Babatunde Kalejaiye <kalejaib@roehampton.ac.uk>
2024-12-05 11:01:44 +0000

    Database Added

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+calculate funtions. This file tests the use of TDD in python
+"""
+
+
+def add(x, y):
+    """
+    add two numbers
+    """
+    return x + y
+
+
+def sub(x, y):
+    """
+    subtract two numbers
+    """
+    return x - y
