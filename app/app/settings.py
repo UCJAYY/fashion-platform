604f22152d70372f02280bb258931be6ab19c0a1
Babatunde Kalejaiye <kalejaib@roehampton.ac.uk>
2024-12-05 10:47:46 +0000

    Added guthub action

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-618i$po(x+ybi&07uig5fuk1i3e((krhl##^wd#00m1n)g&(r0'
+SECRET_KEY = 'django-insecure-618i$po(x+ybi&07uig5fuk1i3e((krhl##^wd#00m1n)g&(r0' # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
